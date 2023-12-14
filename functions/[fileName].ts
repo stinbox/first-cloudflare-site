0bf5848c75f8b9b7bb8a690d5da50e8b30f58183
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-12-15 00:06:28 +0900

    書き方変更

--- functions/[fileName].ts
+++ functions/[fileName].ts
@@ -1,5 +1,9 @@
 export const onRequest: PagesFunction<never, "fileName"> = async (context) => {
-  return fetch(`https://cf-cache-test.stin.ink/${context.params.fileName}`, {
+  const targetURL = new URL(context.request.url);
+
+  targetURL.hostname = "cf-cache-test.stin.ink";
+
+  return fetch(targetURL, {
     headers: context.request.headers,
   });
 };
