1343fcb4145d59d7f5ec7a8d579a29629cebadb7
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-12-14 23:34:54 +0900

    headers を転送する

--- functions/[fileName].ts
+++ functions/[fileName].ts
@@ -1,3 +1,5 @@
 export const onRequest: PagesFunction<never, "fileName"> = async (context) => {
-  return fetch(`https://cf-cache-test.stin.ink/${context.params.fileName}`);
+  return fetch(`https://cf-cache-test.stin.ink/${context.params.fileName}`, {
+    headers: context.request.headers,
+  });
 };
